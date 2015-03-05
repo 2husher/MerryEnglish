c8b732f5f50bd113d08a378084bf35a70a505d14
Alex Izotov <martinandrudy@gmail.com>
2015-03-06 00:41:41 +0300

    added first version of game By One
    Signed-off-by: Alex Izotov <martinandrudy@gmail.com>

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -10,14 +10,47 @@ function sentence(){
   });
 }
 
+var entities;
+var i = 0;
+function by_one(){
+  $("#by_one").on("click", function(){
+    entities = $('div.entity');
+    entities.hide();
+    $('#next_one').show();
+    $('#by_one').hide();
+    entities.eq(i).show();
+    i++;
+  });
+}
+
+function next_one(){
+  $("#next_one").on("click", function(){
+    if (i < entities.length){
+      entities.eq(i-1).hide();
+      entities.eq(i).show();
+      i++;
+    }
+    else{
+      entities.eq(i-1).hide();
+      $('#next_one').hide();
+      i = 0;
+      $('#by_one').show();
+    }
+  });
+}
+
 $(document).ready(function(){
   //alert('ready');
   translate();
   sentence();
+  by_one();
+  next_one();
 });
 
 $(document).on('page:load',function(){
   //alert('load');
   translate();
   sentence();
+  by_one();
+  next_one();
 });
