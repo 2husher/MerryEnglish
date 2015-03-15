b0db1c5374369d85b7c279c3289dd5075aac51b4
Alex Izotov <martinandrudy@gmail.com>
2015-03-15 17:47:38 +0300

    added a lesson creation
    Signed-off-by: Alex Izotov <martinandrudy@gmail.com>

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -4,6 +4,7 @@ class LessonsController < ApplicationController
     #@lessons  = @category.lessons.includes(:entities).all
     @all_lessons = @category.lessons.all
     @lessons     = @all_lessons.paginate(page: params[:page], per_page: 10)
+    @lesson      = @category.lessons.new
   end
 
   def show
@@ -12,4 +13,28 @@ class LessonsController < ApplicationController
     @lesson      = @all_lessons.find(params[:id])
     @entities = @lesson.entities
   end
+
+  def create
+    @category = Category.find(params[:category_id])
+    @lesson   = @category.lessons.build(lesson_params)
+    if @lesson.save
+      respond_to do |format|
+        format.html do
+          redirect_to [@category, @lesson], notice: 'Lesson created'
+        end
+      end
+    else
+      respond_to do |format|
+        format.html do
+          flash[:error] = 'Wrong params for lesson'
+          redirect_to category_lessons_path(@category)
+        end
+      end
+    end
+  end
+
+  private
+    def lesson_params
+      params.require(:lesson).permit(:number)
+    end
 end
